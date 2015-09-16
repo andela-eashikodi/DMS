26a580e2d4ff2e0bac21ba76c95cb40270f4417f
andela-eashikodi <emeka.ashikodi@andela.co>
2015-09-16 11:21:30 +0100

    write query functions

--- documentManager.js
+++ documentManager.js
@@ -1,23 +1,36 @@
-var app = require('express')();
+var models = require('./database');
 var Sequelize = require('sequelize');
-var pg = require('pg-hstore');
-
 var sequelize = new Sequelize('postgres://andela:ashikodi@localhost:5432/test');
 
-sequelize.authenticate().then(function(err) {
-  if (err) {
-    console.log('Connection failed: ', err);
-  }
-  else {
-    console.log('Connection Successful!');
-  }
-});
+module.exports = {
 
-app.listen(5000, function(err) {
-  if(err) {
-    console.log('Server error: ', err);
+  createUser:  function(first, last) {
+    var newUser = {
+      firstName: first,
+      lastName: last
+    };
+    return models.user.create(newUser);
+  },
+  getAllUsers: function() {
+    return models.user.findAll();
+  },
+  createRole: function(title) {
+    return models.role.create({title: title});
+  },
+  getAllRoles: function() {
+    return models.role.findAll();
+  },
+  createDocument: function(title) {
+    return models.doc.create({title: title});
+  },
+  getAllDocuments: function(limit) {
+    return models.doc.findAll({limit: limit, order: 'date_created DESC'});
+  },
+  getAllDocumentsByRole: function(role, limit) {
+    return models.doc.findAll({where: {role: role}, limit: limit, order: 'date_created DESC'});
+  },
+  getAllDocumentsByDate: function(date, limit) {
+    return sequelize.query("SELECT * FROM docs WHERE date_created::date = '" + date + "' LIMIT " + limit);
   }
-  else {
-    console.log('Server started on port 5000');
-  }
-});
+
+};
